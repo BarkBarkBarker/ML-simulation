4245a906f98d2eb6181fdc12b3346ba96852745c
Alexey Burlak <baagsspb@gmail.com>
2021-10-05 01:43:41 +0300

    Fixed description

--- DataGenerator.m
+++ DataGenerator.m
@@ -5,16 +5,17 @@ close all
 % name of file to export data
 file_name = 'data.mat';
 
-% number of points in range of ground and phase resistance
-N = 2;
-
-% auxiliary value for displaying time 
-t_start_iteration = tic;
-
+% arrays to compute
 models = ["GridN1", "GridN3", "GridN2", "GridN4"];
+
 combinations = ["AG", "BG", "CG"];
 
-Ron = logspace(-9,1,N);
-Rg = logspace(-9,1,N);
+Ron = logspace(-9,1,5);
 
-SimFunc.GeneratorCollector(file_name, models, combinations, Ron, Rg, false);
+Rg = logspace(-9,1,5);
+
+N = 20;
+
+SimFunc.GeneratorCollector(file_name, models, combinations, Ron, Rg, false); % pick all possible blocks from every model
+
+SimFunc.GeneratorCollector(file_name, models, combinations, Ron, Rg, true, N); % pick N random blocks from every model
